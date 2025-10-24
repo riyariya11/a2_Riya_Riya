a655b59fbcab47e883813d3cd0bb1edde47fcc44
Riya Riya <riya.riya11@mohawkcollege.ca>
2025-10-24 15:53:17 -0400

    StAuthn added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**StAuth10244: I Riya Riya, 000922180 certify that this material is my original work. 
+No other person's work has been used without due acknowledgement. 
+I have not made my work available to anyone else. */
 let map;
 let markers = [];
 let geocoder;
